5f90b2d4531ec5f8627363257a4a15e92f2a3478
R-Owino <sayeakinyi@gmail.com>
2023-12-03 01:01:33 +0300

    chore: API endpoints definition

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+/* contains the definition of the 2 endpoints: GET /status and GET /stats
+ * GET /status should return if Redis and DB are alive and with a 200 status code
+ * GET /stats should return the number of users and files in DB and a 200 status code
+*/
+
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    const redis = redisClient.isAlive();
+    const db = dbClient.isAlive();
+    res.status(200).send({ redis, db });
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).send({ users, files });
+  }
+}
+
+module.exports = AppController;
