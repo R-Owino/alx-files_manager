1449c187339f993c8d3ad67ed238e0a71daa573c
R-Owino <sayeakinyi@gmail.com>
2023-12-03 23:52:34 +0300

    chore: create a new user in the DB

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -8,11 +8,10 @@ class UsersController {
     const { email, password } = req.body;
     if (!email) return res.status(400).send({ error: 'Missing email' });
     if (!password) return res.status(400).send({ error: 'Missing password' });
-    const userExists = await dbClient.usersCollection.findOne({ email });
-    if (userExists) return res.status(400).send({ error: 'Already exist' });
-    const hashedPassword = sha1(password);
-    const user = await dbClient.usersCollection.insertOne({ email, password: hashedPassword });
-    return res.status(201).send({ id: user.insertedId, email });
+    const userExist = await dbClient.userExists(email);
+    if (userExist) return res.status(400).send({ error: 'Already exist' });
+    const user = await dbClient.createUser(email, sha1(password));
+    return res.status(201).send(user);
   }
 }
 
